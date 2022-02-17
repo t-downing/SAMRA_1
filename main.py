70bdb2e69d762b421323c59352f5122eddab2f25
Tristan Downing <downing.tristan@gmail.com>
2022-02-17 13:22:39 -0500

    new json

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ import json
 def read_downloaded_json(path):
     with open(path) as json_file:
         map_data = json.load(json_file)
-    print(map_data)
     print(f"reading in map name: {map_data['name']}\nwith elements:")
     for element in map_data["elements"]:
-        print(element)
+        print(element["attributes"]["label"])
 
     return map_data
 
 
 def update_element(map_data: dict, label: str, field: str, value):
+    # check validity of field
+
+    if field not in [ex_field["name"] for ex_field in map_data["attributes"]]:
+        print("field not valid, map will not update")
+        return map_data
+    else:
+        print("field valid")
+    # find element
     for element in map_data["elements"]:
         if element["attributes"]["label"] == label:
             element["attributes"][field] = value
@@ -28,6 +35,6 @@ def save_map(map_data, path):
 if __name__ == "__main__":
     load_path = "kumu-tristandowning-samra-1_2.json"
     map_data = read_downloaded_json(load_path)
-    # map_data = update_element(map_data, "Element-1", "field-1", "value-1")
+    map_data = update_element(map_data, "Element-1", "kumu-created-field-1", "value-1")
     save_path = "kumu-tristandowning-samra-1_modified.json"
     save_map(map_data, save_path)
