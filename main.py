8bdfb9b30cd2eababbf92e58596f2006f3efc968
Tristan Downing <downing.tristan@gmail.com>
2022-02-17 15:21:01 -0500

    rename el 2

--- main.py
+++ main.py
@@ -42,5 +42,6 @@ if __name__ == "__main__":
     map_data = read_downloaded_json(load_path)
     map_data = update_element(map_data, "Element-1", "kumu-created-field-1", "value-1")
     map_data = update_element(map_data, "Element-2", "kumu-created-field-1", "value-2")
+    map_data = update_element(map_data, "Element-2", "label", "Element-2-2")
     save_path = "kumu-tristandowning-samra-1_modified_2.json"
     save_map(map_data, save_path)
